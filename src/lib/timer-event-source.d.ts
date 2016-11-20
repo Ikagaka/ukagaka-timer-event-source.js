31489898acd9a0e3017453fc1ac6851a1c3ab29e
Narazaka <info@narazaka.net>
2016-11-20 16:19:36 +0900

    types

---
+++ src/lib/timer-event-source.d.ts
@@ -0,0 +1,12 @@
+import {EventEmitter} from 'events';
+
+/**
+ * タイマーイベントエミッタ
+ * TODO: 将来的にWebWorkerなどにして精度を担保する
+ */
+export class TimerEventSource extends EventEmitter {
+  /**
+   * constructor
+   */
+  constructor();
+}
